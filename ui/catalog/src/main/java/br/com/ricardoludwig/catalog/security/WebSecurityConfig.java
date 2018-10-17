6c95dabac05c77e50d50f2d736efa6a11e3effd8
Ricardo Ludwig <ricardo.ludwig@gmail.com>
2018-10-16 22:13:25 -0300

    #4 Criar front-end do catalago

--- ui/catalog/src/main/java/br/com/ricardoludwig/catalog/security/WebSecurityConfig.java
+++ ui/catalog/src/main/java/br/com/ricardoludwig/catalog/security/WebSecurityConfig.java
@@ -17,7 +17,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .antMatchers("/", "/index", "/js/*", "/css/*", "/fonts/*", "/favicon.ico").permitAll()
+                .antMatchers("/", "/index", "/js/*", "/css/*", "/img/*", "/fonts/*", "/favicon.ico").permitAll()
                 .anyRequest().authenticated()
                 .and()
             .logout()
